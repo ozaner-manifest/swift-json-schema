e1270f0ae419417ad2e0bcf5b2dd7d767371b32d
ozaner-manifest <ozaner@manifestapp.xyz>
2024-10-23 09:14:52 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -26,10 +26,6 @@ let package = Package(
       name: "JSONSchemaClient",
       targets: ["JSONSchemaClient"]
     ),
-    .plugin(
-      name: "JSONSchemaMacro",
-      targets: ["JSONSchemaMacro"]
-    ),
   ],
   dependencies: [
     .package(url: "https://github.com/swiftlang/swift-docc-plugin", from: "1.0.0"),
